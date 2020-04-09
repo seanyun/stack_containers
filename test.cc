970b79be6913851531ca42b5f8ee43942ae44b94
seanyun <seanyun@verbsurgical.com>
2020-04-09 14:52:55 -0700

    Updates the test file to use STL features.

--- test.cc
+++ test.cc
@@ -1,15 +1,15 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 
 #include "stack_container.h"
 
 int main() {
   base::StackVector<int, 10> v;
-  for (size_t i = 0; i < 10; ++i) v->push_back(i);
+  v->resize(10, 0);
+  std::iota(v->begin(), v->end(), 0);
 
-  for (int i : v.container()) {
-    std::cout << i << " ";
-  }
+  for_each(v->begin(), v->end(), [](int i) { std::cout << i << " "; });
   std::cout << std::endl;
 
   return 0;
